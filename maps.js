fd652f8eb106751bc4484d8a50af2e49274bd4b2
Emily Ringoen <emilyaringoen@gmail.com>
2017-02-25 19:35:51 -0700

    changed page layout, added scrolling, add waypoints to map

--- maps.js
+++ maps.js
@@ -1,50 +1,12 @@
 $(document).ready(function() {
 
-  //Start geolocation
-(function getUserLocation() {
-    let pos;
-    let userCords;
-    if (navigator.geolocation) {
-      function error(err) {
-        console.warn('ERROR(' + err.code + '): ' + err.message);
-      }
-
-      function success(pos) {
-        userCords = pos.coords;
-        let location = {
-          lat: userCords.latitude,
-          lng: userCords.longitude
-        }
-        console.log(location)
-          let marker = new google.maps.Marker({
-            position: location,
-            title: 'You are here!'});
-            marker.setMap(map)
-
-            let infoWindowOptions = {
-              content: 'You are here!',
-            }
-            let infoWindow = new google.maps.InfoWindow(infoWindowOptions)
-            google.maps.event.addListener(marker, 'click', function(e){
-              infoWindow.open(map, marker);
-            })
-
-      }
-
-      // Get the user's current position
-      navigator.geolocation.getCurrentPosition(success, error);
-      // console.log(pos.latitude + " " + pos.longitude);
-    } else {
-      alert('Geolocation is not supported in your browser');
-    }
-  })()
 
 
 function makeMap() {
   // Map options
   var mapOptions = {
     zoom: 4,
-    center: new google.maps.LatLng(37.09024, -100.712891),
+    center: new google.maps.LatLng(39.8282, -98.5795),
     panControl: false,
     panControlOptions: {
       position: google.maps.ControlPosition.BOTTOM_LEFT
@@ -58,44 +20,45 @@ function makeMap() {
 
   };
 
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  let map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 makeMap()
 
 // for Autocomplete search bar
-let acOptions = {
-  types: ['establishment']
-}
 
-let autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), acOptions);
-autocomplete.bindTo('bounds', map)
-
-let infoWindow = new google.maps.InfoWindow()
-let marker = new google.maps.Marker({
-  map: map
-})
-
-google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  infoWindow.close()
-  let place = autocomplete.getPlace()
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport)
-  } else {
-    map.setCenter(place.geometry.location)
-    map.setZoom(12)
-  }
-  marker.setPosition(place.geometry.location)
-  infoWindow.setContent('<div><strong>' + place.name + '</strong><br>')
-  infoWindow.open(map, marker)
-  google.maps.event.addListener(marker, 'click', function(e) {
-    infoWindow.open(map, marker)
-  })
-})
 
 
 $('.search').click(function() {
-  let userDestination = $('input').val()
-  console.log(userDestination)
+  let userDestination = $('#start').val()
+  let endDestination = $('#end').val()
+  let waypoint1 = {location: $('#wp1').val()}
+  let waypoint2 = {location: $('#wp2').val()}
+
+  let directionsService = new google.maps.DirectionsService;
+  let directionsDisplay = new google.maps.DirectionsRenderer;
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: 39.8282, lng: -98.5795}
+  });
+  directionsDisplay.setMap(map);
+
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: userDestination,
+      destination: endDestination,
+      waypoints: [waypoint1, waypoint2],
+      travelMode: 'DRIVING'
+    }, function(response, status){
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response)
+      } else {
+        window.alert('Directions request failed due to ' + status)
+      }
+    })
+  }
+
+  calculateAndDisplayRoute(directionsService, directionsDisplay)
 })
 
 
